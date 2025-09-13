08d7daa8e6f173f0194ba0444b822dad170495e1
Faizan Gillani <165652722+faizan-gp@users.noreply.github.com>
2025-09-14 01:06:08 +0800

    GA and meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import AnalyticsClient from "@/components/AnalyticsClient";
 import { Suspense } from "react";
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID
-// const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID
+const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID
 
 const siteUrl = "https://zlwebster.com";
 
@@ -118,7 +118,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <head>
+      <head>
         {GA_ID && (
           <>
             <Script
@@ -136,7 +136,35 @@ export default function RootLayout({
             </Script>
           </>
         )}
-      </head> */}
+
+        {/* Meta Pixel */}
+        {META_PIXEL_ID && (
+          <>
+            <Script id="fb-pixel" strategy="afterInteractive">
+              {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${META_PIXEL_ID}');
+                fbq('track', 'PageView');
+              `}
+            </Script>
+            <noscript>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img height="1" width="1" style={{ display: 'none' }}
+                src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+                alt="" />
+            </noscript>
+          </>
+        )}
+
+
+      </head>
       <body className={`${poppins.variable} ${montserrat.variable} antialiased`}>
         <a href="#main-content" className="sr-only focus:not-sr-only">Skip to content</a>
         <Suspense><AnalyticsClient /></Suspense>
